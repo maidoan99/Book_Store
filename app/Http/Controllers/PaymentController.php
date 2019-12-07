400463628640c9e4c44f266944fe72fbbaefca4b
maidoan99 <maidoan2017@gmail.com>
2019-12-07 18:44:18 +0700

    update create payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -8,6 +8,22 @@ class PaymentController extends Controller
 {
     public function createPayment()
     {
-        return view('main.create_payment');
+        $response = \MoMoAIO::purchase([
+            'amount' => 20000,
+            'returnUrl' => 'http://domaincuaban.com/thanh-toan-thanh-cong/',
+            'notifyUrl' => 'http://domaincuaban.com/ipn/',
+            'orderId' => 'Mã đơn hàng',
+            'requestId' => 'Mã request id, gợi ý nên xài uuid4',
+        ])->send();
+        
+        echo 'send thnah cong';
+        if ($response->isRedirect()) {
+            $redirectUrl = $response->getRedirectUrl();
+            
+            // TODO: chuyển khách sang trang MoMo để thanh toán
+        }
+        
+
+        // return view('main.create_payment');
     }
 }
