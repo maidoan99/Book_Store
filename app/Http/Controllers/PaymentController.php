1ef69f8ed14434ad8c1f5f9642472524a993cbe1
maidoan99 <maidoan2017@gmail.com>
2019-12-08 12:34:25 +0700

    update

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -9,6 +9,9 @@ class PaymentController extends Controller
     public function createPayment()
     {
         $response = \MoMoAIO::purchase([
+            'accessKey' => 'klm05TvNBzhg7h7j',
+            'secretKey' => 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa',
+            'partnerCode' => 'MOMOBKUN20180529',
             'amount' => 20000,
             'returnUrl' => 'https://bookstore-uet.herokuapp.com/home',
             'notifyUrl' => 'https://bookstore-uet.herokuapp.com/ipn/',
