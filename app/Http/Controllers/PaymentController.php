1f753d1509561f939bbd562d67f4e7b3fc147743
maidoan99 <maidoan2017@gmail.com>
2019-12-08 14:34:07 +0700

    update

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -8,16 +8,34 @@ class PaymentController extends Controller
 {
     public function createPayment()
     {
+        $partnerCode = "MOMOQ8CB20191121";
+        $accessKey = "9aGYeOxLihzA2BNK";
+        $serectkey = "271kUhDC2FZEyCSSoegfTY8MQNldnREQ";
+        $requestId = time() . "";
+        $amount = 20000;
+        $orderId = time()."";
+        $orderInfo = 'test thanh toan';
+        $returnUrl = 'https://bookstore-uet.herokuapp.com/home';
+        $notifyurl = 'https://bookstore-uet.herokuapp.com/ipn/';
+        $extraData = 'merchantName=Payment';
+
+        $rawHash = "partnerCode=" . $partnerCode . "&accessKey=" . $accessKey . "&requestId=" . $requestId . "&amount=" . $amount . "&orderId=" . $orderId . "&orderInfo=" . $orderInfo . "&returnUrl=" . $returnUrl . "&notifyUrl=" . $notifyurl . "&extraData=" . $extraData;
+        $signature = hash_hmac("sha256", $rawHash, $serectkey);
+
         $response = \MoMoAIO::purchase([
             'amount' => 20000,
             'returnUrl' => 'https://bookstore-uet.herokuapp.com/home',
             'notifyUrl' => 'https://bookstore-uet.herokuapp.com/ipn/',
-            'orderId' => time() . " ",
-            'requestId' => time() . " ",
+            'orderId' => time() . "",
+            'requestId' => time() . "",
             "partnerCode"=> "MOMOQ8CB20191121",
-                "accessKey"=> "9aGYeOxLihzA2BNK",
-                "secretKey"=> "271kUhDC2FZEyCSSoegfTY8MQNldnREQ",
-                'testMode' => false,
+            "accessKey"=> "9aGYeOxLihzA2BNK",
+            "secretKey"=> "271kUhDC2FZEyCSSoegfTY8MQNldnREQ",
+            'testMode' => false,
+            'orderInfo' => 'test thnah toan qua momo',
+            'requestType' => 'captureMoMoWallet',
+            'signature' => $signature,
+            'extraData' => $extraData
         ])->send();
         
         if ($response->isRedirect()) {
