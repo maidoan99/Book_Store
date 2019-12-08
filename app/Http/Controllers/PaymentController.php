39432350610bfb42e177abc080537f5ec246bb8c
maidoan99 <maidoan2017@gmail.com>
2019-12-08 18:51:06 +0700

    update

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,29 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 
 class PaymentController extends Controller
 {
     public function createPayment(Request $request)
     {
-        $response = \MoMoAIO::purchase([
-            'amount' => 20000,
-            'returnUrl' => 'https://bookstore-uet.herokuapp.com/thanh-toan-thanh-cong/',
-            'notifyUrl' => 'https://bookstore-uet.herokuapp.com/ipn/',
-            'orderId' => time(),
-            'requestId' => time()
-        ])->send();
+        // $response = \MoMoAIO::purchase([
+        //     'amount' => 20000,
+        //     'returnUrl' => 'https://bookstore-uet.herokuapp.com/thanh-toan-thanh-cong/',
+        //     'notifyUrl' => 'https://bookstore-uet.herokuapp.com/ipn/',
+        //     'orderId' => time(),
+        //     'requestId' => time()
+        // ])->send();
         
-        if ($response->isRedirect()) {
-            echo "tao yeu cau thanh cong";
-            $redirectUrl = $response->getRedirectUrl();
-            return redirect($redirectUrl);
+        // if ($response->isRedirect()) {
+        //     echo "tao yeu cau thanh cong";
+        //     $redirectUrl = $response->getRedirectUrl();
+        //     return redirect($redirectUrl);
             
-            // TODO: chuyển khách sang trang MoMo để thanh toán
-        } else{
-            echo $response->localMessage;
-            echo "<br>";
-            echo $response->message;
-        }
+        //     // TODO: chuyển khách sang trang MoMo để thanh toán
+        // } else{
+        //     echo $response->localMessage;
+        //     echo "<br>";
+        //     echo $response->message;
+        // }
+
+        return Config::get('laravel-ominipay.MoMoAIO.partnerCode');
     }
 }
