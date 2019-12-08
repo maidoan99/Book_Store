5f8174909da5219b234ea544bbbf1ac2243e62c3
maidoan99 <maidoan2017@gmail.com>
2019-12-08 16:47:18 +0700

    update

--- config/laravel-omnipay.php
+++ config/laravel-omnipay.php
@@ -8,9 +8,9 @@ return [
         'MoMoAIO' => [
             'driver' => 'MoMo_AllInOne',
             'options' => [
-                "partnerCode"=> "MOMOIQA420180417",
-                "accessKey"=> "Q8gbQHeDesB2Xs0t",
-                "secretKey"=> "PPuDXq1KowPT1ftR8DvlQTHhC03aul17",
+                "partnerCode"=> "",
+                "accessKey"=> "",
+                "secretKey"=> "",
                 'testMode' => false,
             ],
         ],
