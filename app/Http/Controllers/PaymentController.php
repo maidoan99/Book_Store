c20998081b4cde6e54671aec9c1ae64d1e65ad7a
maidoan99 <maidoan2017@gmail.com>
2019-12-08 11:06:06 +0700

    update

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -8,20 +8,21 @@ class PaymentController extends Controller
 {
     public function createPayment()
     {
-        $response = \MoMoAIO::purchase([
-            'amount' => 20000,
-            'returnUrl' => 'https://bookstore-uet.herokuapp.com/thanh-toan-thanh-cong/',
-            'notifyUrl' => 'https://bookstore-uet.herokuapp.com/ipn/',
-            'orderId' => time(),
-            'requestId' => time(),
-        ])->send();
+        // $response = \MoMoAIO::purchase([
+        //     'amount' => 20000,
+        //     'returnUrl' => 'https://bookstore-uet.herokuapp.com/thanh-toan-thanh-cong/',
+        //     'notifyUrl' => 'https://bookstore-uet.herokuapp.com/ipn/',
+        //     'orderId' => time(),
+        //     'requestId' => time(),
+        // ])->send();
         
-        if ($response->isRedirect()) {
-            $redirectUrl = $response->getRedirectUrl();
-            echo 'send thanh cong';
-            return view('main.create_payment');
-        } else{
-            echo $response->message;
-        }
+        // if ($response->isRedirect()) {
+        //     $redirectUrl = $response->getRedirectUrl();
+        //     echo 'send thanh cong';
+        //     return view('main.create_payment');
+        // } else{
+        //     echo $response->message;
+        // }
+        return view('main.create_payment');
     }
 }
