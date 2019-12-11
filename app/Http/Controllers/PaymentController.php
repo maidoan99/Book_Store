d6578437aca4a61504f636dedf0b2919f729c369
maidoan99 <maidoan2017@gmail.com>
2019-12-11 22:02:12 +0700

    update

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -14,16 +14,12 @@ class PaymentController extends Controller
             'returnUrl' => 'https://bookstore-uet.herokuapp.com/thanh-toan-thanh-cong/',
             'notifyUrl' => 'https://bookstore-uet.herokuapp.com/ipn/',
             'orderId' => time(),
-            'requestId' => time(),
-            "partnerCode"=> "MOMOIQA420180417",
-            "accessKey"=> "Q8gbQHeDesB2Xs0t",
-            "secretKey"=> "PPuDXq1KowPT1ftR8DvlQTHhC03aul17",
+            'requestId' => time()
         ])->send();
         
         if ($response->isRedirect()) {
-            echo "tao yeu cau thanh cong";
             $redirectUrl = $response->getRedirectUrl();
-            return redirect($redirectUrl);
+            return redirect()->to($redirectUrl);
             
             // TODO: chuyển khách sang trang MoMo để thanh toán
         } else{
